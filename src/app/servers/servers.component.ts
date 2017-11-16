ef6ecb5c5bd35f06e1b2a20ad2577751aac6b5fd
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-16 14:56:12 +0000

    Completed lesson 21 - Observed that @Component selector is actually a css selector.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // Selector can take any standard CSS selector other than #id - recommended to just use an element though for components.
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
